b890e62794b32a7568b1a070e988ac4123fedcd8
Jan <janstanczuk95@gmail.com>
2021-09-30 14:26:40 +0100

    removing jax dependence

--- datasets.py
+++ datasets.py
@@ -17,7 +17,7 @@
 """Return training and evaluation/test datasets from config files."""
 #import jax
 import tensorflow as tf
-import tensorflow_datasets as tfds
+#import tensorflow_datasets as tfds
 
 
 def get_data_scaler(config):
