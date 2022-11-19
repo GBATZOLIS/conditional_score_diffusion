e4df244d621f9b467e280587d97fbfb19990fa0c
GBATZOLIS <gb511@cam.ac.uk>
2022-11-19 17:54:17 +0000

    mm

--- run_lib.py
+++ run_lib.py
@@ -478,7 +478,7 @@ def get_manifold_dimension(config):
 
   num_batches = ambient_dim // batchsize + 1
   extra_in_last_batch = ambient_dim - (ambient_dim // batchsize) * batchsize
-  num_batches *= 8
+  num_batches *= 4
 
   t = eps
   vec_t = torch.ones(x.size(0), device=device) * t
