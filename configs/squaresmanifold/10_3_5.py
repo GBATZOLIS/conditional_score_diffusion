30a396bfa7dec02ecebc4cc3b16f545508d8650e
GBATZOLIS <gb511@cam.ac.uk>
2023-01-19 00:16:49 +0000

    ddpm

--- configs/squaresmanifold/10_3_5.py
+++ configs/squaresmanifold/10_3_5.py
@@ -92,7 +92,7 @@ def get_config():
   data.dataset_type = 'SquaresManifold'
   data.create_dataset = False
   data.split = [0.8, 0.1, 0.1]
-  data.data_samples = 300000
+  data.data_samples = 3000
   data.image_size = 32
   data.effective_image_size = data.image_size
   data.centered = False
