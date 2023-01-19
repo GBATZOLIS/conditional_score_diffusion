e2db3f96cf2a4319c152f0e76ae425fbf11bba25
GBATZOLIS <gb511@cam.ac.uk>
2023-01-19 00:45:09 +0000

    ddpm

--- configs/squaresmanifold/20_3_5.py
+++ configs/squaresmanifold/20_3_5.py
@@ -40,7 +40,7 @@ def get_config():
   training.accelerator = None if training.gpus <= 1 else 'ddp'
   training.accumulate_grad_batches = 1
   training.lightning_module = 'base' 
-  config.training.batch_size = 32 #256
+  config.training.batch_size = 256
   training.workers = 4
   training.num_epochs = 10000
   training.n_iters = 2500000
@@ -92,7 +92,7 @@ def get_config():
   data.dataset_type = 'SquaresManifold'
   data.create_dataset = False
   data.split = [0.8, 0.1, 0.1]
-  data.data_samples = 3000
+  data.data_samples = 300000
   data.image_size = 32
   data.effective_image_size = data.image_size
   data.centered = False
