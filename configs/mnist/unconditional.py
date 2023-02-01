436ddf78f0deebf94311ca9839940be8ce5d23f0
GBATZOLIS <gb511@cam.ac.uk>
2023-02-01 15:04:56 +0100

    ff

--- configs/mnist/unconditional.py
+++ configs/mnist/unconditional.py
@@ -40,7 +40,7 @@ def get_config():
   training.accelerator = None if training.gpus <= 1 else 'ddp'
   training.accumulate_grad_batches = 1
   training.lightning_module = 'base' 
-  config.training.batch_size = 256
+  config.training.batch_size = 16 #256
   training.workers = 4
   training.num_epochs = 10000
   training.n_iters = 2500000
@@ -107,7 +107,7 @@ def get_config():
   
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = None #'/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/dimension_detection/experiments/fixed_gaussians_manifold/20/checkpoints/best/last.ckpt'
+  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/dimension_detection/experiments/mnist/unconditional/checkpoints/best/last.ckpt'
   model.sigma_min = 0.01
   model.sigma_max = 50
   model.num_scales = 1000
