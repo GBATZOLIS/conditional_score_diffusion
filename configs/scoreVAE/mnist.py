2bc1f469520214359d6a822a2320e50e5bdb8cea
GBATZOLIS <gb511@cam.ac.uk>
2023-02-09 15:32:35 +0000

    fix eval batchsize

--- configs/scoreVAE/mnist.py
+++ configs/scoreVAE/mnist.py
@@ -65,7 +65,7 @@ def get_config():
   evaluate.workers = training.workers
   evaluate.begin_ckpt = 50
   evaluate.end_ckpt = 96
-  evaluate.batch_size = 1
+  evaluate.batch_size = validation.batch_size
   evaluate.enable_sampling = True
   evaluate.num_samples = 50000
   evaluate.enable_loss = True
