d11a80422f2a5879442a0864c7610d828b982db3
GBATZOLIS <gb511@cam.ac.uk>
2023-02-09 14:55:29 +0000

    hh

--- configs/scoreVAE/mnist.py
+++ configs/scoreVAE/mnist.py
@@ -33,7 +33,7 @@ def get_config():
   training.eval_freq = 2500
   #------              --------
   
-  training.visualization_callback = 'base'
+  training.visualization_callback = None
   training.show_evolution = False
 
   training.likelihood_weighting = True
