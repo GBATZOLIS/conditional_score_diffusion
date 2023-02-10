2e2e7f8b69dd90e7474ef559f13a1589881b7795
Jan Stanczuk <js2164@login-e-4.data.cluster>
2023-02-10 19:54:30 +0000

    update

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from job_master.run_configs import run_configs
 
 configs = [
-    'configs/sanity_check.py'
+    'configs/ksphere/robustness/0.py',
+    'configs/ksphere/robustness/0_25.py',
+    'configs/ksphere/robustness/0_5.py',
+    'configs/ksphere/robustness/0_75.py',
+    'configs/ksphere/robustness/1.py',
 ]
 
 run_configs(configs)
