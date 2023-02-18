1e0be3f7f0694df6e8a34331b21d1e74c5653955
GBATZOLIS <gb511@cam.ac.uk>
2023-02-18 01:44:05 +0000

    continue training

--- configs/scoreVAE/mnist.py
+++ configs/scoreVAE/mnist.py
@@ -94,7 +94,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = None 
+  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/scoreVAE/experiments/mnist/modified_loss_ampere_tbatch_1_correct_optimizer/checkpoints/best/last.ckpt' 
   model.sigma_min = 0.01
   model.sigma_max = 50
   model.num_scales = 1000
