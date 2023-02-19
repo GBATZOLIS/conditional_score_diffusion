deb1c2c20e1178fac26f1feab739ac21d8ef9a1a
GBATZOLIS <gb511@cam.ac.uk>
2023-02-19 22:31:16 +0000

    mirror decoder

--- models/decoder.py
+++ models/decoder.py
@@ -16,6 +16,190 @@ get_act = layers.get_act
 get_normalization = normalization.get_normalization
 default_initializer = layers.default_init
 
+class MirrorDecoder(pl.LightningModule):
+    def __init__(self, config):
+        """
+        Inputs:
+            - num_input_channels : Number of channels of the image to reconstruct. For CIFAR, this parameter is 3
+            - base_channel_size : Number of channels we use in the last convolutional layers. Early layers might use a duplicate of it.
+            - latent_dim : Dimensionality of latent representation z
+            - act_fn : Activation function used throughout the decoder network
+        """
+        super().__init__()
+        num_input_channels = config.model.encoder_input_channels
+        base_channel_size = config.model.encoder_base_channel_size
+        latent_dim = config.model.encoder_latent_dim
+        act_fn = nn.GELU
+        c_hid = base_channel_size
+        self.linear = nn.Sequential(
+            nn.Linear(latent_dim, 2*16*c_hid),
+            act_fn()
+        )
+        self.net = nn.Sequential(
+            nn.ConvTranspose2d(2*c_hid, 2*c_hid, kernel_size=3, output_padding=1, padding=1, stride=2), # 4x4 => 8x8
+            act_fn(),
+            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.ConvTranspose2d(2*c_hid, c_hid, kernel_size=3, output_padding=1, padding=1, stride=2), # 8x8 => 16x16
+            act_fn(),
+            nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.ConvTranspose2d(c_hid, num_input_channels, kernel_size=3, output_padding=1, padding=1, stride=2), # 16x16 => 32x32
+        )
+
+        #nn.Tanh() # The input images is scaled between -1 and 1, hence the output has to be bounded as well
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = x.reshape(x.shape[0], -1, 4, 4)
+        x = self.net(x)
+        return x
+
+@utils.register_model(name='ddpm_mirror_decoder')
+class DDPMdecoder(pl.LightningModule):
+  def __init__(self, config):
+    super().__init__()
+    self.act = act = get_act(config)
+
+    self.nf = nf = config.model.nf
+    ch_mult = config.model.ch_mult
+    self.num_res_blocks = num_res_blocks = config.model.num_res_blocks
+    self.attn_resolutions = attn_resolutions = config.model.attn_resolutions
+    dropout = config.model.dropout
+    resamp_with_conv = config.model.resamp_with_conv
+    self.num_resolutions = num_resolutions = len(ch_mult)
+    self.all_resolutions = all_resolutions = [config.data.effective_image_size // (2 ** i) for i in range(num_resolutions)] #80,40,20,10
+
+    AttnBlock = functools.partial(layers.AttnBlock)
+    self.conditional = conditional = config.model.conditional
+    ResnetBlock = functools.partial(ResnetBlockDDPM, act=act, temb_dim=4 * nf, dropout=dropout)
+    if conditional:
+      # Condition on noise levels.
+      modules = [nn.Linear(nf, nf * 4)] #changed it to incorporate the latent dimension - we removed it in this version of the decoder.
+      modules[0].weight.data = default_initializer()(modules[0].weight.data.shape)
+      nn.init.zeros_(modules[0].bias)
+      modules.append(nn.Linear(nf * 4, nf * 4))
+      modules[1].weight.data = default_initializer()(modules[1].weight.data.shape)
+      nn.init.zeros_(modules[1].bias)
+
+    self.centered = config.data.centered
+    input_channels = config.model.input_channels
+    output_channels = config.model.output_channels
+
+    # ddpm_conv3x3
+    modules.append(conv3x3(input_channels, nf))
+    hs_c = [nf]
+    in_ch = nf
+    for i_level in range(num_resolutions):
+      # Residual blocks for this resolution
+      for i_block in range(num_res_blocks):
+        out_ch = nf * ch_mult[i_level]
+        modules.append(ResnetBlock(in_ch=in_ch, out_ch=out_ch))
+        in_ch = out_ch
+        if all_resolutions[i_level] in attn_resolutions:
+          modules.append(AttnBlock(channels=in_ch))
+        hs_c.append(in_ch)
+      if i_level != num_resolutions - 1:
+        modules.append(Downsample(channels=in_ch, with_conv=resamp_with_conv))
+        hs_c.append(in_ch)
+
+    in_ch = hs_c[-1]
+    modules.append(ResnetBlock(in_ch=in_ch))
+    modules.append(AttnBlock(channels=in_ch))
+    modules.append(ResnetBlock(in_ch=in_ch))
+
+    # Upsampling block
+    for i_level in reversed(range(num_resolutions)):
+      for i_block in range(num_res_blocks + 1):
+        out_ch = nf * ch_mult[i_level]
+        modules.append(ResnetBlock(in_ch=in_ch + hs_c.pop(), out_ch=out_ch))
+        in_ch = out_ch
+      if all_resolutions[i_level] in attn_resolutions:
+        modules.append(AttnBlock(channels=in_ch))
+      if i_level != 0:
+        modules.append(Upsample(channels=in_ch, with_conv=resamp_with_conv))
+
+    assert not hs_c
+    modules.append(nn.GroupNorm(num_channels=in_ch, num_groups=32, eps=1e-6))
+    modules.append(conv3x3(in_ch, output_channels, init_scale=0.))
+    self.all_modules = nn.ModuleList(modules)
+
+    self.mirror_decoder = MirrorDecoder(config)
+
+  def forward(self, batch, labels):
+    out_x=batch['x']
+    latent=batch['y']
+
+    decoded_latent = self.mirror_decoder(latent)
+    x = torch.cat((out_x, decoded_latent), dim=1)
+    
+    modules = self.all_modules
+    m_idx = 0
+    if self.conditional:
+      # timestep/scale embedding
+      timesteps = labels
+      timesteps = layers.get_timestep_embedding(timesteps, self.nf)
+
+      temb = modules[m_idx](temb)
+      m_idx += 1
+      temb = modules[m_idx](self.act(temb))
+      m_idx += 1     
+      
+    else:
+      temb = None
+
+    if self.centered:
+      # Input is in [-1, 1]
+      h = x
+    else:
+      # Input is in [0, 1]
+      h = 2 * x - 1.
+
+    # Downsampling block
+    hs = [modules[m_idx](h)]
+    m_idx += 1
+    for i_level in range(self.num_resolutions):
+      # Residual blocks for this resolution
+      for i_block in range(self.num_res_blocks):
+        h = modules[m_idx](hs[-1], temb)
+        m_idx += 1
+        if h.shape[-1] in self.attn_resolutions:
+          h = modules[m_idx](h)
+          m_idx += 1
+        hs.append(h)
+      if i_level != self.num_resolutions - 1:
+        hs.append(modules[m_idx](hs[-1]))
+        m_idx += 1
+
+    h = hs[-1]
+    h = modules[m_idx](h, temb)
+    m_idx += 1
+    h = modules[m_idx](h)
+    m_idx += 1
+    h = modules[m_idx](h, temb)
+    m_idx += 1
+
+    # Upsampling block
+    for i_level in reversed(range(self.num_resolutions)):
+      for i_block in range(self.num_res_blocks + 1):
+        h = modules[m_idx](torch.cat([h, hs.pop()], dim=1), temb)
+        m_idx += 1
+      if h.shape[-1] in self.attn_resolutions:
+        h = modules[m_idx](h)
+        m_idx += 1
+      if i_level != 0:
+        h = modules[m_idx](h)
+        m_idx += 1
+
+    assert not hs
+    h = self.act(modules[m_idx](h))
+    m_idx += 1
+    h = modules[m_idx](h)
+    m_idx += 1
+    assert m_idx == len(modules)
+
+    return h
+
 @utils.register_model(name='ddpm_decoder')
 class DDPMdecoder(pl.LightningModule):
   def __init__(self, config):
