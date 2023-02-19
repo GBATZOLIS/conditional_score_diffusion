f8196594075c86a0f9d74f49924b36f4cf4f41ed
GBATZOLIS <gb511@cam.ac.uk>
2023-02-19 18:20:02 +0000

    try simpler encoder

--- models/encoder.py
+++ models/encoder.py
@@ -139,3 +139,38 @@ class DDPMencoder(pl.LightningModule):
     assert m_idx == len(modules)
 
     return h
+
+@utils.register_model(name='simple_encoder')
+class Encoder(pl.LightningModule):
+    def __init__(self, config):
+        """
+        Inputs:
+            - num_input_channels : Number of input channels of the image. For CIFAR, this parameter is 3
+            - base_channel_size : Number of channels we use in the first convolutional layers. Deeper layers might use a duplicate of it.
+            - latent_dim : Dimensionality of latent representation z
+            - act_fn : Activation function used throughout the encoder network
+        """
+        super().__init__()
+        num_input_channels = config.model.encoder_input_channels
+        base_channel_size = config.model.encoder_base_channel_size
+        latent_dim = config.model.encoder_latent_dim
+        act_fn = nn.GELU
+
+        c_hid = base_channel_size
+        self.net = nn.Sequential(
+            nn.Conv2d(num_input_channels, c_hid, kernel_size=3, padding=1, stride=2), # 32x32 => 16x16
+            act_fn(),
+            nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 16x16 => 8x8
+            act_fn(),
+            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 8x8 => 4x4
+            act_fn(),
+            nn.Flatten(), # Image grid to single feature vector
+            nn.Linear(2*16*c_hid, latent_dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
