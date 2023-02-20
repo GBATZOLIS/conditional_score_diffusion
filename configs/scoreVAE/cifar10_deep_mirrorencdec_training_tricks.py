3b180e2f753d40e1f00b37e4bd07529adc6e5ff9
GBATZOLIS <gb511@cam.ac.uk>
2023-02-20 16:27:50 +0000

    training and sampling tricks

--- configs/scoreVAE/cifar10_deep_mirrorencdec_training_tricks.py
+++ configs/scoreVAE/cifar10_deep_mirrorencdec_training_tricks.py
@@ -10,7 +10,7 @@ def get_config():
   #logging
   config.logging = logging = ml_collections.ConfigDict()
   logging.log_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/scoreVAE/experiments/cifar10/'
-  logging.log_name = '384_simple_encoder_mirror_decoder'
+  logging.log_name = 'deep_mirror_encdec_train_sample_improvements'
   logging.top_k = 5
   logging.every_n_epochs = 1000
   logging.envery_timedelta = timedelta(minutes=1)
