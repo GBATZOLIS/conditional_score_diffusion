0941af461d4a7ebbe4eac950157703c7a8a8c283
GBATZOLIS <gb511@cam.ac.uk>
2023-02-22 00:21:10 +0000

    continue]

--- configs/scoreVAE/cifar10_deep_mirrorencdec_training_tricks.py
+++ configs/scoreVAE/cifar10_deep_mirrorencdec_training_tricks.py
@@ -96,7 +96,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = None
+  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/scoreVAE/experiments/cifar10/deep_mirror_encdec_train_sample_improvements/checkpoints/best/last.ckpt'
   model.sigma_min = 0.01
   model.sigma_max = 50
   model.num_scales = 1000
