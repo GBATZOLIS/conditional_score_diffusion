1b56c4b3adb95ff42e29fab9189d0ba41a4852ab
GBATZOLIS <gb511@cam.ac.uk>
2023-02-25 22:25:43 +0000

    better evaluation frequency

--- configs/scoreVAE/paper/celebA/celebA_ae.py
+++ configs/scoreVAE/paper/celebA/celebA_ae.py
@@ -35,7 +35,7 @@ def get_config():
   training.eval_freq = 2500
   #------              --------
   
-  training.visualisation_freq = 50
+  training.visualisation_freq = 3
   training.visualization_callback = None
   training.show_evolution = False
 
@@ -98,7 +98,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = None
+  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/scoreVAE/experiments/paper/celebA-HQ/ae/checkpoints/best/last.ckpt'
   model.sigma_min = 0.01
   model.sigma_max = 50
   model.num_scales = 1000
