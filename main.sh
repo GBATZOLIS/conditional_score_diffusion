e4967c23a9750618128b12a382225e921af1f249
Teo Deveney <td314@bath.ac.uk>
2023-03-03 17:29:35 +0000

    small network FP on density experiments

--- main.sh
+++ main.sh
@@ -31,4 +31,4 @@ nvidia-smi
 source /home/td491/.bashrc
 conda activate scoresde_env
 cd /home/td491/rds/hpc-work/conditional_score_diffusion
-python main.py --config configs/jan/circles/fokker_planck/1_3_256.py --mode train
+python main.py --config configs/jan/circles/fokker_planck/1e2_small.py --mode train
