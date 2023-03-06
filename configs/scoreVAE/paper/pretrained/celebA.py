741f163411823f8aab4de85afdfcba8b4681bb0e
GBATZOLIS <gb511@cam.ac.uk>
2023-03-06 23:26:53 +0000

    f

--- configs/scoreVAE/paper/pretrained/celebA.py
+++ configs/scoreVAE/paper/pretrained/celebA.py
@@ -97,7 +97,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = None
+  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/scoreVAE/experiments/paper/pretrained/celebA-HQ/ae_ema/checkpoints/best/last.ckpt'
   model.sigma_min = 0.01
   model.sigma_max = 50
   model.num_scales = 1000
