35eeaca9dfbf7f82caf3017cdb8436f140b6f940
GBATZOLIS <gb511@cam.ac.uk>
2023-03-15 17:36:20 +0000

    train a pretrained plus aux model on celebA 64

--- models/encoder.py
+++ models/encoder.py
@@ -194,14 +194,22 @@ class Encoder(pl.LightningModule):
               act_fn(),
               nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
               act_fn(),
+
               nn.Conv2d(c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 32x32 => 16x16
               act_fn(),
               nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
               act_fn(),
-              nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 8x8 => 4x4
+
+              nn.Conv2d(2*c_hid, 4*c_hid, kernel_size=3, padding=1, stride=2), # 16x16 => 8x8
               act_fn(),
+              nn.Conv2d(4*c_hid, 4*c_hid, kernel_size=3, padding=1),
+              act_fn(),
+
+              nn.Conv2d(4*c_hid, 4*c_hid, kernel_size=3, padding=1, stride=2), # 8x8 => 4x4
+              act_fn(),
+
               nn.Flatten(), # Image grid to single feature vector
-              nn.Linear(2*16*c_hid, out_dim)
+              nn.Linear(4*16*c_hid, out_dim)
           )
 
         elif config.data.image_size == 128:
@@ -230,8 +238,7 @@ class Encoder(pl.LightningModule):
             act_fn(),
 
             nn.Flatten(), # Image grid to single feature vector
-            nn.Linear(8*16*c_hid, out_dim),
-            nn.Tanh()
+            nn.Linear(8*16*c_hid, out_dim)
         )
 
     def forward(self, x):
