493924f16ed1d1f1a51acdb23832e7a530fb147d
GBATZOLIS <gb511@cam.ac.uk>
2023-03-16 00:42:52 +0000

    only encoder for celebA

--- models/encoder.py
+++ models/encoder.py
@@ -293,7 +293,27 @@ class Encoder(pl.LightningModule):
           )
 
           self.linear = nn.Linear(2*16*c_hid+1, out_dim)
-
+      
+        elif config.data.image_size == 64:
+          self.net = nn.Sequential(
+              nn.Conv2d(num_input_channels, c_hid, kernel_size=3, padding=1, stride=2), # 64x64 => 32x32
+              act_fn(),
+              nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
+              act_fn(),
+              nn.Conv2d(c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 32x32 => 16x16
+              act_fn(),
+              nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
+              act_fn(),
+              nn.Conv2d(2*c_hid, 4*c_hid, kernel_size=3, padding=1, stride=2), # 16x16 => 8x8
+              act_fn(),
+              nn.Conv2d(4*c_hid, 4*c_hid, kernel_size=3, padding=1),
+              act_fn(),
+              nn.Conv2d(4*c_hid, 4*c_hid, kernel_size=3, padding=1, stride=2), # 8x8 => 4x4
+              act_fn(),
+              nn.Flatten(), # Image grid to single feature vector
+            )
+          self.linear = nn.Linear(4*16*c_hid+1, out_dim)
+      
     def forward(self, x, t):
         flattened_img = self.net(x)
         concat = torch.cat((flattened_img, t[:, None]), dim=1)
