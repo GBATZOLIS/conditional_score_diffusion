03a2145613ae2f365c11e46a4950b9b5045d3bf7
GBATZOLIS <gb511@cam.ac.uk>
2023-04-02 18:15:51 +0300

    train celebA on ResNet encoder

--- configs/scoreVAE/paper/pretrained/celebA_64/VAE/ResNetEncoder.py
+++ configs/scoreVAE/paper/pretrained/celebA_64/VAE/ResNetEncoder.py
@@ -38,7 +38,7 @@ def get_config():
   training.eval_freq = 2500
   #------              --------
   
-  training.visualisation_freq = 75
+  training.visualisation_freq = 25
   training.visualization_callback = None
   training.show_evolution = False
 
