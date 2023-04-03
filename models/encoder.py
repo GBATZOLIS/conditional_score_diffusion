ba98040e8bfdea21172fb9f0325c1f49394c4c37
GBATZOLIS <gb511@cam.ac.uk>
2023-04-03 18:20:38 +0300

    train VAE encoders on both celebA and cifar10 using the new ResNet design

--- models/encoder.py
+++ models/encoder.py
@@ -451,3 +451,140 @@ class DDPMEncoder(pl.LightningModule):
       return h
 
 
+@utils.register_model(name='time_dependent_half_U_encoder')
+class HalfUEncoder(pl.LightningModule):
+  def __init__(self, config):
+    super().__init__()
+    self.latent_dim = latent_dim = config.encoder.latent_dim
+
+    #extra logic
+    self.variational = config.training.variational
+    if hasattr(config.model, 'encoder_split_output'):
+      self.split_output = config.encoder.split_output
+    else:
+      self.split_output = True
+    
+    if self.variational:
+      out_dim = 2*latent_dim
+    else:
+      out_dim = latent_dim
+
+    self.act = act = get_act(config)
+
+    self.nf = nf = config.encoder.nf
+    ch_mult = config.encoder.ch_mult
+    self.num_res_blocks = num_res_blocks = config.encoder.num_res_blocks
+    self.attn_resolutions = attn_resolutions = config.encoder.attn_resolutions
+    dropout = config.encoder.dropout
+    resamp_with_conv = config.encoder.resamp_with_conv
+    self.num_resolutions = num_resolutions = len(ch_mult)
+    self.all_resolutions = all_resolutions = [config.data.effective_image_size // (2 ** i) for i in range(num_resolutions)] #80,40,20,10
+
+    AttnBlock = functools.partial(layers.AttnBlock)
+    self.conditional = conditional = config.encoder.conditional
+    ResnetBlock = functools.partial(ResnetBlockDDPM, act=act, temb_dim=4 * nf, dropout=dropout)
+    if conditional:
+      # Condition on noise levels.
+      modules = [nn.Linear(nf, nf * 4)]
+      modules[0].weight.data = default_initializer()(modules[0].weight.data.shape)
+      nn.init.zeros_(modules[0].bias)
+      modules.append(nn.Linear(nf * 4, nf * 4))
+      modules[1].weight.data = default_initializer()(modules[1].weight.data.shape)
+      nn.init.zeros_(modules[1].bias)
+    else:
+      modules = []
+
+    self.centered = config.data.centered
+    input_channels = config.encoder.input_channels
+    output_channels = config.encoder.output_channels
+
+    if self.latent_dim:
+        self.last_hidden_dim = self.all_resolutions[-1]**2 * output_channels
+        self.latent_projection = nn.Linear(self.last_hidden_dim, 2 * self.latent_dim)
+
+    # ddpm_conv3x3
+    modules.append(conv3x3(input_channels, nf))
+    hs_c = [nf]
+    in_ch = nf
+    for i_level in range(num_resolutions):
+      # Residual blocks for this resolution
+      for i_block in range(num_res_blocks):
+        out_ch = nf * ch_mult[i_level]
+        modules.append(ResnetBlock(in_ch=in_ch, out_ch=out_ch))
+        in_ch = out_ch
+        if all_resolutions[i_level] in attn_resolutions:
+          modules.append(AttnBlock(channels=in_ch))
+        hs_c.append(in_ch)
+      if i_level != num_resolutions - 1:
+        modules.append(Downsample(channels=in_ch, with_conv=resamp_with_conv))
+        hs_c.append(in_ch)
+
+    in_ch = hs_c[-1]
+    modules.append(ResnetBlock(in_ch=in_ch))
+    modules.append(AttnBlock(channels=in_ch))
+    modules.append(ResnetBlock(in_ch=in_ch))
+
+    modules.append(nn.GroupNorm(num_channels=in_ch, num_groups=32, eps=1e-6))
+    modules.append(conv3x3(in_ch, output_channels, init_scale=0.))
+    self.all_modules = nn.ModuleList(modules)
+
+  def forward(self, x, labels=None):
+    modules = self.all_modules
+    m_idx = 0
+    if self.conditional:
+      # timestep/scale embedding
+      timesteps = labels
+      temb = layers.get_timestep_embedding(timesteps, self.nf)
+      temb = modules[m_idx](temb)
+      m_idx += 1
+      temb = modules[m_idx](self.act(temb))
+      m_idx += 1
+    else:
+      temb = None
+
+    if self.centered:
+      # Input is in [-1, 1]
+      h = x
+    else:
+      # Input is in [0, 1]
+      h = 2 * x - 1.
+
+    # Downsampling block
+    hs = [modules[m_idx](h)]
+    m_idx += 1
+    for i_level in range(self.num_resolutions):
+      # Residual blocks for this resolution
+      for i_block in range(self.num_res_blocks):
+        h = modules[m_idx](hs[-1], temb)
+        m_idx += 1
+        if h.shape[-1] in self.attn_resolutions:
+          h = modules[m_idx](h)
+          m_idx += 1
+        hs.append(h)
+      if i_level != self.num_resolutions - 1:
+        hs.append(modules[m_idx](hs[-1]))
+        m_idx += 1
+
+    h = hs[-1]
+    h = modules[m_idx](h, temb)
+    m_idx += 1
+    h = modules[m_idx](h)
+    m_idx += 1
+    h = modules[m_idx](h, temb)
+    m_idx += 1
+
+    h = self.act(modules[m_idx](h))
+    m_idx += 1
+    h = modules[m_idx](h)
+    m_idx += 1
+    assert m_idx == len(modules)
+    
+    if self.latent_dim:
+        h = h.reshape((h.shape[0], -1))
+        h = self.latent_projection(h)
+
+    #extra logic
+    if self.variational and self.split_output:
+      return h[:,:self.latent_dim], h[:,self.latent_dim:]
+    else:
+      return h
