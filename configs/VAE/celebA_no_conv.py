36b147ca071879fc7b0cbed1957e6390bf98b2d9
Jan <janstanczuk95@gmail.com>
2023-04-10 14:18:25 +0100

    no_conv_VAE

---
+++ configs/VAE/celebA_no_conv.py
@@ -0,0 +1,124 @@
+import ml_collections
+import torch
+import math
+import numpy as np
+from datetime import timedelta
+
+def get_config():
+  config = ml_collections.ConfigDict()
+
+  # logging
+  config.logging = logging = ml_collections.ConfigDict()
+  logging.log_freq = 5
+
+  # training
+  config.training = training = ml_collections.ConfigDict()
+  training.batch_size = 128
+  training.workers = 4
+
+  # validation
+  config.validation = validation = ml_collections.ConfigDict()
+  validation.batch_size = 128
+  validation.workers = 4
+  
+  # test
+  config.eval = evaluate = ml_collections.ConfigDict()
+  evaluate.batch_size = validation.batch_size
+  evaluate.workers = 4
+  
+  # data
+  config.data = data = ml_collections.ConfigDict()
+  data.base_dir = '/store/CIA/js2164/data'
+  data.dataset = 'celeba'
+  data.task = 'generation'
+  data.datamodule = 'unpaired_PKLDataset'
+  data.scale = 4 
+  data.use_data_mean = False
+  data.create_dataset = False
+  data.split = [0.8, 0.1, 0.1]
+  data.image_size = 64
+  data.effective_image_size = data.image_size
+  data.shape = [3, data.image_size, data.image_size]
+  data.centered = False
+  data.use_flip = True
+  data.crop = True
+  data.uniform_dequantization = False
+  data.num_channels = data.shape[0] 
+
+  # model
+  config.model = model = ml_collections.ConfigDict()
+  model.nonlinearity = 'swish'
+  model.latent_dim = 512
+  data.latent_dim = model.latent_dim
+  model.kl_weight = 0.01
+
+  # encoder model
+  config.encoder = encoder = ml_collections.ConfigDict()
+  encoder.name = 'time_dependent_DDPM_encoder'
+
+  model.checkpoint_path = None
+  model.sigma_min = 0.01
+  model.sigma_max = 50
+  model.num_scales = 1000
+  model.beta_min = 0.1
+  model.beta_max = 20.
+  model.dropout = 0.
+  model.embedding_type = 'fourier'
+
+  model.unconditional_score_model_name = 'ddpm'
+  model.name = 'ddpm_mirror_decoder'
+  model.input_channels = data.num_channels
+  model.output_channels = data.num_channels
+  model.scale_by_sigma = True
+  model.ema_rate = 0.9999
+  model.normalization = 'GroupNorm'
+  model.nonlinearity = 'swish'
+  model.nf = 128
+  model.ch_mult = (1, 1, 2, 2, 3)
+  model.num_res_blocks = 2
+  model.attn_resolutions = (16,)
+  model.resamp_with_conv = True
+  model.conditional = False
+  model.fir = True
+  model.fir_kernel = [1, 3, 3, 1]
+  model.skip_rescale = True
+  model.resblock_type = 'biggan'
+  model.progressive = 'none'
+  model.progressive_input = 'residual'
+  model.progressive_combine = 'sum'
+  model.attention_type = 'ddpm'
+  model.init_scale = 0.
+  model.fourier_scale = 16
+  model.conv_size = 3
+
+
+  # decoder model
+  config.decoder = decoder = ml_collections.ConfigDict()
+  decoder.name = 'half_U_decoder_no_conv'
+  decoder.scale_by_sigma = False
+  decoder.ema_rate = 0.9999
+  decoder.dropout = 0.0
+  decoder.normalization = 'GroupNorm'
+  decoder.nonlinearity = model.nonlinearity
+  decoder.nf = 128
+  decoder.ch_mult = (1, 1, 2, 2, 3)
+  decoder.num_res_blocks = 2
+  decoder.attn_resolutions = (16,)
+  decoder.resamp_with_conv = True
+  decoder.conditional = False
+  decoder.init_scale = 0.
+  decoder.embedding_type = 'positional'
+  decoder.conv_size = 3
+  decoder.input_channels = model.ch_mult[-1] * model.nf
+  decoder.output_channels = data.num_channels
+  decoder.latent_dim = model.latent_dim
+
+  # optimization
+  config.optim = optim = ml_collections.ConfigDict()
+  optim.lr = 1e-4
+  optim.use_scheduler=True
+  optim.sch_factor = 0.25
+  optim.sch_patience = 5
+  optim.sch_min_lr = 1e-5
+
+  return config
--- models/half_U.py
+++ models/half_U.py
@@ -279,3 +279,106 @@ class HalfUDecoder(pl.LightningModule):
     assert m_idx == len(modules)
 
     return h
+  
+
+@utils.register_model(name='half_U_decoder_no_conv')
+class HalfUDecoderNoConv(pl.LightningModule):
+  def __init__(self, config):
+    super().__init__()
+    self.act = act = get_act(config)
+    self.latent_dim = config.model.latent_dim
+
+    self.nf = nf = config.decoder.nf
+    ch_mult = config.decoder.ch_mult
+    self.num_res_blocks = num_res_blocks = config.decoder.num_res_blocks
+    self.attn_resolutions = attn_resolutions = config.decoder.attn_resolutions
+    dropout = config.decoder.dropout
+    resamp_with_conv = config.decoder.resamp_with_conv
+    self.num_resolutions = num_resolutions = len(ch_mult)
+    self.all_resolutions = all_resolutions = [config.data.effective_image_size // (2 ** i) for i in range(num_resolutions)] #80,40,20,10
+
+
+    AttnBlock = functools.partial(layers.AttnBlock)
+    self.conditional = conditional = config.decoder.conditional
+    ResnetBlock = functools.partial(ResnetBlockDDPM, act=act, temb_dim=4 * nf, dropout=dropout)
+    if conditional:
+      # Condition on noise levels.
+      modules = [nn.Linear(nf, nf * 4)]
+      modules[0].weight.data = default_initializer()(modules[0].weight.data.shape)
+      nn.init.zeros_(modules[0].bias)
+      modules.append(nn.Linear(nf * 4, nf * 4))
+      modules[1].weight.data = default_initializer()(modules[1].weight.data.shape)
+      nn.init.zeros_(modules[1].bias)
+    else:
+      modules = []
+
+    self.centered = config.data.centered
+    self.input_channels = config.decoder.input_channels
+    output_channels = config.decoder.output_channels
+    
+    if self.latent_dim:
+        self.last_hidden_dim = self.all_resolutions[-1]**2 * self.input_channels
+        self.latent_projection = nn.Linear(self.latent_dim, self.last_hidden_dim)
+
+    # ddpm_conv3x3
+    #modules.append(conv3x3(self.input_channels, nf))
+    in_ch = self.input_channels
+   # Upsampling block
+    for i_level in reversed(range(num_resolutions)):
+      for i_block in range(num_res_blocks + 1):
+        out_ch = nf * ch_mult[i_level]
+        modules.append(ResnetBlock(in_ch=in_ch, out_ch=out_ch))
+        in_ch = out_ch
+      if all_resolutions[i_level] in attn_resolutions:
+        modules.append(AttnBlock(channels=in_ch))
+      if i_level != 0:
+        modules.append(Upsample(channels=in_ch, with_conv=resamp_with_conv))
+
+    modules.append(nn.GroupNorm(num_channels=in_ch, num_groups=32, eps=1e-6))
+    modules.append(conv3x3(in_ch, output_channels, init_scale=0.))
+    self.all_modules = nn.ModuleList(modules)
+
+  def forward(self, x, labels=None):
+
+    if self.latent_dim:
+       h = self.latent_projection(x)
+       h = h.reshape(h.shape[0], self.input_channels, self.all_resolutions[-1], self.all_resolutions[-1])
+    else:
+       h=x
+
+    modules = self.all_modules
+    m_idx = 0
+    if self.conditional:
+      # timestep/scale embedding
+      timesteps = labels
+      temb = layers.get_timestep_embedding(timesteps, self.nf)
+      temb = modules[m_idx](temb)
+      m_idx += 1
+      temb = modules[m_idx](self.act(temb))
+      m_idx += 1
+    else:
+      temb = None
+
+
+    #h = modules[m_idx](h)
+    #m_idx += 1
+
+    # Upsampling block
+    for i_level in reversed(range(self.num_resolutions)):
+      for i_block in range(self.num_res_blocks + 1):
+        h = modules[m_idx](h, temb)
+        m_idx += 1
+      if h.shape[-1] in self.attn_resolutions:
+        h = modules[m_idx](h)
+        m_idx += 1
+      if i_level != 0:
+        h = modules[m_idx](h)
+        m_idx += 1
+
+    h = self.act(modules[m_idx](h))
+    m_idx += 1
+    h = modules[m_idx](h)
+    m_idx += 1
+    assert m_idx == len(modules)
+
+    return h
