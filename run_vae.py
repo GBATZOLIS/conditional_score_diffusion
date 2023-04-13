701f4896f51ed60642e78c123f33d248bcc02e9e
Jan Stanczuk <js2164@login-e-1.data.cluster>
2023-04-13 12:14:49 +0100

    update

--- run_vae.py
+++ run_vae.py
@@ -6,8 +6,9 @@ from configs.utils import read_config
 from pathlib import Path
 
 configs = [
-            'configs/VAE/celebA.py',
-            'configs/VAE/cifar.py',
+            #'configs/VAE/celebA.py',
+            #'configs/VAE/cifar.py',
+            'configs/VAE/celebA_no_conv.py'
         ]
            
 
@@ -22,7 +23,7 @@ for config_path in configs:
         os.makedirs(output_path)
 
     create_mainsh(main_sh_path = main_sh_path,
-                  file_path='vae_main .py',
+                  file_path='vae_main.py',
                   args_dict={'config': config_path},
                   job_name=f'VAE_{config.data.dataset}_kl_{config.model.kl_weight}',
                   partition='ampere',
