9cb402bb262f0e41198e173d843759e1d25fbca4
GBATZOLIS <gb511@cam.ac.uk>
2023-04-19 14:57:17 +0300

    st

--- configs/scoreVAE/paper/pretrained/celebA_64/VAE/CorrectedResNetEncoder.py
+++ configs/scoreVAE/paper/pretrained/celebA_64/VAE/CorrectedResNetEncoder.py
@@ -108,7 +108,7 @@ def get_config():
   # model
   config.model = model = ml_collections.ConfigDict()
   model.use_config_translator = False
-  model.checkpoint_path = None
+  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/scoreVAE/experiments/paper/pretrained/celebA_64/corrected_only_ResNetEncoder_VAE_KLweight_0.01/checkpoints/best/last.ckpt'
   model.sigma_min = 0.01
   model.sigma_max = 50
   model.num_scales = 1000
