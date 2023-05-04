391459088b9547d9d8dc41a74657ec9155ae9d8d
GBATZOLIS <gb511@cam.ac.uk>
2023-05-04 15:35:41 +0100

    t slice svd estimatino of mnist - digit 1

--- dim_reduction.py
+++ dim_reduction.py
@@ -34,75 +34,84 @@ def get_conditional_manifold_dimension(config, name=None):
   score_fn = mutils.get_score_fn(sde, score_model, conditional=False, train=False, continuous=True)
   #---- end of setup ----
 
-  num_datapoints = config.get('dim_estimation.num_datapoints', 2501)
-  singular_values = []
-  labels = []
-  imgs = []
-  idx = 0
-  with tqdm(total=num_datapoints) as pbar:
-    for orig_batch, orig_labels in train_dataloader:
-      #orig_batch = orig_batch.to(device)
-      batchsize = orig_batch.size(0)
-      
-      if idx+1 >= num_datapoints:
-          break
+  num_datapoints = config.get('dim_estimation.num_datapoints', 26)
+
+  times = torch.linspace(pl_module.sampling_eps, 0.3, 12)
+  for t_slice in times:
+    t_save_path = os.path.join(log_path, log_name, 'svd', '%.3f' % t_slice.item())
+    Path(t_save_path).mkdir(parents=True, exist_ok=True)
+
+    singular_values = []
+    labels = []
+    imgs = []
+    idx = 0
+    with tqdm(total=num_datapoints) as pbar:
+      for orig_batch, orig_labels in train_dataloader:
+        #orig_batch = orig_batch.to(device)
+        batchsize = orig_batch.size(0)
         
-      for x, y in zip(orig_batch, orig_labels):
         if idx+1 >= num_datapoints:
-          break
-        
-        imgs.append(x.permute(1, 2, 0))
-        x = x.to(device)
-        ambient_dim = np.prod(x.shape[1:])
-        x = x.repeat([batchsize,]+[1 for i in range(len(x.shape))])
+            break
+          
+        for x, y in zip(orig_batch, orig_labels):
+          if y.item() != 1:
+            continue
 
-        num_batches = ambient_dim // batchsize + 1
-        extra_in_last_batch = ambient_dim - (ambient_dim // batchsize) * batchsize
-        num_batches *= 4
+          if idx+1 >= num_datapoints:
+            break
+          
+          imgs.append(x.permute(1, 2, 0))
+          x = x.to(device)
+          ambient_dim = np.prod(x.shape[1:])
+          x = x.repeat([batchsize,]+[1 for i in range(len(x.shape))])
 
-        t = 10*pl_module.sampling_eps
-        vec_t = torch.ones(x.size(0), device=device) * t
+          num_batches = ambient_dim // batchsize + 1
+          extra_in_last_batch = ambient_dim - (ambient_dim // batchsize) * batchsize
+          num_batches *= 4
 
-        scores = []
-        for i in tqdm(range(1, num_batches+1)):
-          batch = x.clone()
+          t = t_slice #10*pl_module.sampling_eps
+          vec_t = torch.ones(x.size(0), device=device) * t
 
-          mean, std = sde.marginal_prob(batch, vec_t)
-          z = torch.randn_like(batch)
-          batch = mean + std[(...,) + (None,) * len(batch.shape[1:])] * z
-          score = score_fn(batch, vec_t).detach().cpu()
+          scores = []
+          for i in tqdm(range(1, num_batches+1)):
+            batch = x.clone()
 
-          if i < num_batches:
-            scores.append(score)
-          else:
-            scores.append(score[:extra_in_last_batch])
-        
-        scores = torch.cat(scores, dim=0)
-        scores = torch.flatten(scores, start_dim=1)
+            mean, std = sde.marginal_prob(batch, vec_t)
+            z = torch.randn_like(batch)
+            batch = mean + std[(...,) + (None,) * len(batch.shape[1:])] * z
+            score = score_fn(batch, vec_t).detach().cpu()
 
-        means = scores.mean(dim=0, keepdim=True)
-        normalized_scores = scores - means
+            if i < num_batches:
+              scores.append(score)
+            else:
+              scores.append(score[:extra_in_last_batch])
+          
+          scores = torch.cat(scores, dim=0)
+          scores = torch.flatten(scores, start_dim=1)
 
-        u, s, v = torch.linalg.svd(normalized_scores)
-        s = s.tolist()
-        singular_values.append(s)
-        labels.append(y.item())
+          means = scores.mean(dim=0, keepdim=True)
+          normalized_scores = scores - means
 
-        idx+=1
-        pbar.update(1)
+          u, s, v = torch.linalg.svd(normalized_scores)
+          s = s.tolist()
+          singular_values.append(s)
+          labels.append(y.item())
 
-  imgs = torch.stack(imgs).numpy()
-  with open(os.path.join(save_path, 'images.pkl'), 'wb') as f:
-    info = {'images':imgs}
-    pickle.dump(info, f)
+          idx+=1
+          pbar.update(1)
 
-  with open(os.path.join(save_path, 'labels_svd.pkl'), 'wb') as f:
-    info = {'singular_values':singular_values}
-    pickle.dump(info, f)
-  
-  with open(os.path.join(save_path, 'labels.pkl'), 'wb') as f:
-    info = {'labels': labels}
-    pickle.dump(info, f)
+    imgs = torch.stack(imgs).numpy()
+    with open(os.path.join(t_save_path, 'images.pkl'), 'wb') as f:
+      info = {'images':imgs}
+      pickle.dump(info, f)
+
+    with open(os.path.join(t_save_path, 'labels_svd.pkl'), 'wb') as f:
+      info = {'singular_values':singular_values}
+      pickle.dump(info, f)
+    
+    with open(os.path.join(t_save_path, 'labels.pkl'), 'wb') as f:
+      info = {'labels': labels}
+      pickle.dump(info, f)
 
 def get_manifold_dimension(config, name=None, return_svd=False):
   #---- create the setup ---
