22b115e620f43262fb99a5ef63f358c2b73d5798
GBATZOLIS <gb511@cam.ac.uk>
2023-07-05 03:49:44 +0300

    test ScoreVAE in the continuous adaptation

--- configs/scoreVAE/paper/pretrained/cifar10/cifar10_gd_encoder.py
+++ configs/scoreVAE/paper/pretrained/cifar10/cifar10_gd_encoder.py
@@ -39,7 +39,7 @@ def get_config():
   training.eval_freq = 2500
   #------              --------
   
-  training.visualisation_freq = 50
+  training.visualisation_freq = 1
   training.visualization_callback = None
   training.show_evolution = False
 
