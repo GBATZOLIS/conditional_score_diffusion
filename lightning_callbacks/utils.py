6ea37199ee3fb7af6f300a2d5e3f8504cfe28280
GBATZOLIS <gb511@cam.ac.uk>
2023-08-05 16:54:36 +0300

    ll

--- lightning_callbacks/utils.py
+++ lightning_callbacks/utils.py
@@ -46,7 +46,7 @@ def get_callbacks(config):
     if config.training.visualization_callback is not None:
       if isinstance(config.training.visualization_callback, list):
         for callback in config.training.visualization_callback:
-          if callback in ['celeba_distribution_shift', 'jan_georgios']:
+          if callback in ['encoder_contribution', 'celeba_distribution_shift', 'jan_georgios']:
             callbacks.append(get_callback_by_name(callback)(config))
           else:
             callbacks.append(get_callback_by_name(callback)(show_evolution=config.training.show_evolution))
