e8bfab0faff213ec357a41e885697b610bd9f1d9
GBATZOLIS <gb511@cam.ac.uk>
2023-08-07 11:31:24 +0300

    check code

--- run_lib.py
+++ run_lib.py
@@ -65,11 +65,11 @@ def train(config, log_path, checkpoint_path, log_name=None):
                           num_sanity_val_steps=0
                           )
     
-    cuda_available = torch.cuda.is_available()
-    map_location = torch.device('cuda') if cuda_available else torch.device('cpu')
+    #cuda_available = torch.cuda.is_available()
+    #map_location = torch.device('cuda') if cuda_available else torch.device('cpu')
 
-    if checkpoint_path:
-      LightningModule = LightningModule.load_from_checkpoint(checkpoint_path, config=config, map_location=map_location)
+    #if checkpoint_path:
+    #  LightningModule = LightningModule.load_from_checkpoint(checkpoint_path, config=config, map_location=map_location)
       
     trainer.fit(LightningModule, datamodule=DataModule, ckpt_path=checkpoint_path)
 
