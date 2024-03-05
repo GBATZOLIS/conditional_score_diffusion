c2f80063844118596d41766af821bbaa6df587e5
GBATZOLIS <gb511@cam.ac.uk>
2024-03-05 12:36:51 +0000

    branch cleaning

--- notebooks/nb_utils.py
+++
@@ -1,8 +0,0 @@
-import os 
-import sys
-from pathlib import Path
-repo_path=Path('.').absolute().parent.absolute()
-print(f'Moving cwd to {repo_path}')
-os.chdir(repo_path)
-sys.path.append(repo_path)
-import run_lib
--- send_to_hpc.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m 'update'
-git push
-ssh hpc 'cd /home/js2164/rds/hpc-work/repos/score_sde_pytorch; git pull'
